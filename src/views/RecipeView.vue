528b1e70ae0099b06e47b163cef7a7d34fc40f66
xiao ching <carol.xiao.ching@gmail.com>
2025-04-01 13:00:00 +0800

    first commit

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -15,7 +15,7 @@
         </ol>
       </nav>
     </div>
-    <div class="col-12 col-md-6 col-xl-7 mb-11 mb-md-16" v-if="recipe.coverImgUrl">
+    <div class="col-12 col-md-6 col-xl-7 mb-11 mb-md-16">
       <div
         class="recipeCover backgroundCover w-100 h-100 rounded-2"
         :style="{ backgroundImage: `url(${recipe.coverImgUrl})` }"
@@ -87,7 +87,7 @@
         <span class="me-6"> 食材 </span>
         <span class="fs-5 text-muted">({{ recipe.servings }} 人份)</span>
       </h2>
-      <ul class="row list-unstyled mb-0" v-if="recipe._id">
+      <ul class="row list-unstyled mb-0">
         <li
           class="col-12 col-sm-6"
           v-for="(ingredientItem, index) in recipe.ingredients"
@@ -154,7 +154,7 @@
         <span> 步驟 </span>
       </h2>
       <div class="step mx-auto">
-        <ul class="stepList list-unstyled position-relative m-0 p-0" v-if="recipe._id">
+        <ul class="stepList list-unstyled position-relative m-0 p-0">
           <li
             class="stepItem position-relative"
             v-for="(stepItem, index) in recipe.steps"
